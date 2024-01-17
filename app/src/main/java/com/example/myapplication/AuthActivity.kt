15247c1592da5b6a618230f27a516e046be3cac7
Aksonof <Aksonof1@gmail.com>
2024-01-17 14:03:22 +0200

    commit before review - added comments

--- app/src/main/java/com/example/myapplication/AuthActivity.kt
+++ app/src/main/java/com/example/myapplication/AuthActivity.kt
@@ -1,9 +1,11 @@
 package com.example.myapplication
 
+import android.app.ActivityOptions
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.os.Handler
+import android.os.HandlerThread
 import android.text.TextUtils
 import android.util.Patterns
 import android.view.KeyEvent
@@ -21,28 +23,33 @@ class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
-        val userEmail =
-            findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.emailEditText)
-        val userPass =
-            findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.passEditText)
-        val registerButton =
-            findViewById<androidx.appcompat.widget.AppCompatButton>(R.id.registerButton)
-        val intent = Intent(this, MainActivity::class.java)
-
-        val checkBox = findViewById<CheckBox>(R.id.rememberMeCheckBox)
-        sharedPref = getSharedPreferences("autoLog", MODE_PRIVATE)
-
-        userEmail.setText(sharedPref.getString("email", ""))
-        userPass.setText(sharedPref.getString("pass", ""))
 
         var email: String
         var pass: String
+        val emailView =
+            findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.emailEditText)
+        val passView =
+            findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.passEditText)
+        val registerButton =
+            findViewById<androidx.appcompat.widget.AppCompatButton>(R.id.registerButton)
+        val checkBox = findViewById<CheckBox>(R.id.rememberMeCheckBox)
+        val intent = Intent(this, MainActivity::class.java)
+        sharedPref = getSharedPreferences("autoLog", MODE_PRIVATE)
 
+        emailView.setText(sharedPref.getString("email", ""))
+        passView.setText(sharedPref.getString("pass", ""))
+
+        /*
+        * This code handles the registration button click. It reads email and password input,
+        * checks if they are correct, saves them in SharedPreferences if a checkbox is checked,
+        * and starts a new activity with custom animation. If the data is invalid,
+        * it displays an error toast.
+        * */
         registerButton.setOnClickListener {
-            email = userEmail.text.toString()
-            pass = userPass.text.toString()
+            email = emailView.text.toString()
+            pass = passView.text.toString()
 
-            if (isValidEmail(userEmail.text.toString()) && isValidPass(userPass.text.toString())) {
+            if (isValidEmail(email) && isValidPass(pass)) {
                 if (checkBox.isChecked) {
                     val editor = sharedPref.edit()
                     editor.putString("email", email)
@@ -50,8 +57,9 @@ class AuthActivity : AppCompatActivity() {
                     editor.apply()
                 }
                 intent.putExtra("email", email)
-                startActivity(intent)
-                overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+                val options =
+                    ActivityOptions.makeCustomAnimation(this, R.anim.fade_in, R.anim.fade_out)
+                startActivity(intent, options.toBundle())
             } else {
                 Toast.makeText(
                     this,
@@ -60,94 +68,105 @@ class AuthActivity : AppCompatActivity() {
             }
         }
 
-        userEmail.setOnEditorActionListener { _, actionId, event ->
+        /*
+        * This code handles the "Done" or "Enter" click. It validates the entered email, displays
+        * an error message if it's invalid, and returns true.
+        * If the action is not "Done" or "Enter," it returns false.
+        * */
+        emailView.setOnEditorActionListener { _, actionId, event ->
             if (actionId == EditorInfo.IME_ACTION_DONE ||
                 (event.action == KeyEvent.ACTION_DOWN && event.keyCode == KeyEvent.KEYCODE_ENTER)
             ) {
-                email = userEmail.text.toString()
-                if (isValidEmail(email)) {
-                    true
-                } else {
-                    userEmail.error = "Use the correct email format"
-                    true
+                if (!isValidEmail(emailView.text.toString())) {
+                    emailView.error = "Use the correct email format"
                 }
+                true
             } else {
                 false
             }
         }
 
-        userEmail.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
+        /*
+        * When the field loses focus, it checks if the entered email is valid.
+        * If not, it sets an error message.
+        * */
+        emailView.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
             if (!hasFocus) {
-                email = userEmail.text.toString()
-                if (isValidEmail(email)) {
-                    // to do
-                } else {
-                    userEmail.error = "Use the correct email format"
+                if (!isValidEmail(emailView.text.toString())) {
+                    emailView.error = "Use the correct email format"
                 }
             }
         }
 
-
-        userPass.setOnEditorActionListener { _, actionId, event ->
-
-            val passLayout =
-                findViewById<TextInputLayout>(R.id.passInputLayout)
-
+        /*
+        * This code handles the "Done" or "Enter" click. It validates the entered pass,
+        * removes the icon password toggle for 6 seconds to show the password format and then
+        * sets it back.
+        * If the action is not "Done" or "Enter," it returns false.
+        * */
+        passView.setOnEditorActionListener { _, actionId, event ->
             if (actionId == EditorInfo.IME_ACTION_DONE ||
                 (event.action == KeyEvent.ACTION_DOWN && event.keyCode == KeyEvent.KEYCODE_ENTER)
             ) {
-                pass = userPass.text.toString()
-
-                if (isValidPass(pass)) {
-                    true
-                } else {
-
-                    val handler = Handler()
-                    passLayout.endIconMode = TextInputLayout.END_ICON_NONE
-                    userPass.error = "Password: 6+ chars, Aa-Zz, 0-9"
-
-                    handler.postDelayed({
-                        userPass.error = null
-                        passLayout.endIconMode = TextInputLayout.END_ICON_PASSWORD_TOGGLE
-                    }, 4000)
-
-
-                    true
-                }
-            } else {
-                false
-            }
-        }
-
-        userPass.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
-            if (!hasFocus) {
-                pass = userPass.text.toString()
-                if (isValidPass(pass)) {
-                    // to do
-                } else {
-                    val handler = Handler()
+                if (!isValidPass(passView.text.toString())) {
                     val passLayout =
                         findViewById<TextInputLayout>(R.id.passInputLayout)
+                    val handlerThread = HandlerThread("MyHandlerThread")
+                    handlerThread.start()
+                    val handler = Handler(handlerThread.looper)
                     passLayout.endIconMode = TextInputLayout.END_ICON_NONE
-
-                    userPass.error = "Password: 6+ chars, Aa-Zz, 0-9"
+                    passView.error = "Password: 6+ chars, Aa-Zz, 0-9"
                     handler.postDelayed({
-                        userPass.error = null
+                        passView.error = null
                         passLayout.endIconMode = TextInputLayout.END_ICON_PASSWORD_TOGGLE
-                    }, 4000)
+
+                    }, 6000)
+                    handlerThread.quitSafely()
+                }
+                true
+            } else {
+                false
+            }
+        }
+
+        /*
+        * When the field loses focus, it validates the entered pass, removes the icon password
+        * toggle for 6 seconds to show the password format and then sets it back.
+        * */
+        passView.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus) {
+                if (!isValidPass(passView.text.toString())) {
+                    val passLayout =
+                        findViewById<TextInputLayout>(R.id.passInputLayout)
+                    val handlerThread = HandlerThread("MyHandlerThread")
+                    handlerThread.start()
+                    val handler = Handler(handlerThread.looper)
+                    passLayout.endIconMode = TextInputLayout.END_ICON_NONE
+                    passView.error = "Password: 6+ chars, Aa-Zz, 0-9"
+                    handler.postDelayed({
+                        passView.error = null
+                        passLayout.endIconMode = TextInputLayout.END_ICON_PASSWORD_TOGGLE
+                    }, 6000)
+                    handlerThread.quitSafely()
                 }
             }
         }
     }
 
-
+    /*
+    * It uses a regular expression to ensure that the password contains at least 6 characters,
+    * including a mix of uppercase and lowercase letters (Aa-Zz) and numeric digits (0-9).
+    * */
     private fun isValidPass(pass: String): Boolean {
         val regex = Regex("^[A-Za-z0-9]{6,}$")
         return regex.matches(pass)
     }
 
+    /*
+    *  It returns true if the email is not empty and matches the standard email address pattern;
+    * otherwise, it returns false.
+    * */
     private fun isValidEmail(email: String): Boolean {
         return (!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches())
     }
-
 }
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,16 +8,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val email = intent.extras?.getString("email") ?: "Username"
         val name = findViewById<TextView>(R.id.name)
         name.text = getNameFromEmail(email)
-
     }
 
+    /*
+    * It removes numeric digits from the email and processes the local part before the '@' symbol.
+    * If the local part doesn't contain underscores or dots, it returns the unmodified local part.
+    *
+    * If the local part contains underscores or dots, it separates it into a first name and
+    * last name, replaces underscores with dots, and capitalizes the first letter of each part.
+    * Finally, it combines the first and last names and returns the full name.
+    * */
     private fun getNameFromEmail(email: String): String {
 
         val emailWithOutNumbers = email.replace(Regex("\\d"), "")
+
+        if (!Regex("[._]").matches(emailWithOutNumbers.substringBefore('@'))) {
+            return emailWithOutNumbers.substringBefore('@')
+        }
 
         var firstName = emailWithOutNumbers.substringBefore('@')
             .replace('_', '.')
