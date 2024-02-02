baabd764d3392f8d0f3c549ec07c67d87c8ca472
Aksonof <Aksonof1@gmail.com>
2024-02-02 15:16:39 +0200

    task-1: pull request changes done

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,16 +1,35 @@
 package com.example.myapplication
 
+import android.app.ActivityOptions
+import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val email = intent.extras?.getString("email") ?: "Username"
-        val name = findViewById<TextView>(R.id.name)
-        name.text = getNameFromEmail(email)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        processUserName()
+        logOutClickListener()
+    }
+
+    private fun logOutClickListener() {
+        binding.logOutViewButton.setOnClickListener {
+            val intent = Intent(this, AuthActivity::class.java)
+            val options =
+                ActivityOptions.makeCustomAnimation(this, R.anim.fade_in, R.anim.fade_out)
+            intent.putExtra("logOut", true)
+            startActivity(intent, options.toBundle())
+        }
+    }
+
+    private fun processUserName() {
+        val email = intent.extras?.getString("email") ?: "John Doe"
+        binding.userNameTextView.text = getNameFromEmail(email)
     }
 
     /*
@@ -23,21 +42,23 @@ class MainActivity : AppCompatActivity() {
     * */
     private fun getNameFromEmail(email: String): String {
 
-        val emailWithOutNumbers = email.replace(Regex("\\d"), "")
+        val name = email
+            .replace(Regex("\\d"), "")
+            .substringBefore('@')
+            .replace('_', '.')
 
-        if (!Regex("[._]").matches(emailWithOutNumbers.substringBefore('@'))) {
-            return emailWithOutNumbers.substringBefore('@')
+        if (name.isEmpty()) {
+            return "John Doe"
+        } else if (!name.contains(".")) {
+            return name.replaceFirstChar { Character.toUpperCase(name[0]) }
         }
 
-        var firstName = emailWithOutNumbers.substringBefore('@')
-            .replace('_', '.')
-            .substringBefore('.')
+        var firstName = name.substringBefore('.')
         firstName = firstName.replaceFirstChar { Character.toUpperCase(firstName[0]) }
 
-        var lastName = emailWithOutNumbers.substringBefore('@')
-            .replace('_', '.')
-            .substringAfter('.')
+        var lastName = name.substringAfter('.')
         lastName = lastName.replaceFirstChar { Character.toUpperCase(lastName[0]) }
+
         return "$firstName $lastName"
     }
 
