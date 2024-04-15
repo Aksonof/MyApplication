b496976caa25eefd7f92288555c34118e69e6858
Aksonof <Aksonof1@gmail.com>
2024-04-15 19:32:09 +0300

    task_3: layouts for fragments in progress

---
+++ app/src/main/java/com/example/myapplication/fragments/MyProfileFragment.kt
@@ -0,0 +1,26 @@
+package com.example.myapplication.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.myapplication.databinding.FragmentMyProfileBinding
+
+class MyProfileFragment : Fragment() {
+
+    private lateinit var binding: FragmentMyProfileBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMyProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+
+
+}
