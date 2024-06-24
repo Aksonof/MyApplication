be7102210d44d5f12a8c322c9c5873ede8e4df7d
Aksonof <Aksonof1@gmail.com>
2024-06-24 22:04:57 +0300

    .idea git info exclude + merge dev → task_4

--- app/src/main/java/com/example/myapplication/adapter/UserActionListener.kt
+++
@@ -1,8 +0,0 @@
-package com.example.myapplication.adapter
-
-import com.example.myapplication.model.User
-
-interface UserActionListener {
-    fun onDeleteUser(user: User)
-    fun onUserDetails(user: User)
-}
