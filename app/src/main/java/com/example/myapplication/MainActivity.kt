f3e1d19baf3b870957694aab9360c6c41fd6fc86
Aksonof <Aksonof1@gmail.com>
2024-07-02 21:40:53 +0300

    Changes after the code review.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,52 +1,6 @@
 package com.example.myapplication
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-
-
-    /*
-        private fun logOutClickListener() {
-            binding.logOutViewButton.setOnClickListener {
-                val intent = Intent(this, AuthActivity::class.java)
-                val options =
-                    ActivityOptions.makeCustomAnimation(this, R.anim.fade_in, R.anim.fade_out)
-                intent.putExtra("logOut", true)
-                startActivity(intent, options.toBundle())
-            }
-        }
-
-        private fun processUserName() {
-            val email = intent.extras?.getString("email") ?: "John Doe"
-            binding.userNameTextView.text = getNameFromEmail(email)
-        }
-
-        private fun getNameFromEmail(email: String): String {
-
-            val name = email
-                .replace(Regex("\\d"), "")
-                .substringBefore('@')
-                .replace('_', '.')
-
-            if (name.isEmpty()) {
-                return "John Doe"
-            } else if (!name.contains(".")) {
-                return name.replaceFirstChar { Character.toUpperCase(name[0]) }
-            }
-
-            var firstName = name.substringBefore('.')
-            firstName = firstName.replaceFirstChar { Character.toUpperCase(firstName[0]) }
-
-            var lastName = name.substringAfter('.')
-            lastName = lastName.replaceFirstChar { Character.toUpperCase(lastName[0]) }
-
-            return "$firstName $lastName"
-        }*/
-
-}
--- app/src/main/java/com/example/myapplication/adapter/UsersAdapter.kt
+++ app/src/main/java/com/example/myapplication/adapter/UsersAdapter.kt
@@ -1,10 +1,9 @@
 package com.example.myapplication.adapter
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -19,62 +18,58 @@ private const val MULTISELECT_MODE_MARGIN = 40
 class UsersAdapter(private val actionListener: UserActionListener) :
     ListAdapter<User, UsersAdapter.UsersViewHolder>(MyItemCallback()) {
 
-    private val _isModeActive = MutableLiveData(false)
-    private val isModeActive: LiveData<Boolean> get() = _isModeActive
+    private var isModeActive: Boolean = false
 
+    @SuppressLint("NotifyDataSetChanged")
     fun changeModeStatus(state: Boolean) {
-        _isModeActive.value = state
+        isModeActive = state
+        notifyDataSetChanged()
     }
 
-    class UsersViewHolder(
+    inner class UsersViewHolder(
         private val binding: UserPatternBinding,
-        private val actionListener: UserActionListener,
-        private val isModeActive: LiveData<Boolean>,
-        private val setModeActive: (Boolean) -> Unit
     ) : RecyclerView.ViewHolder(binding.root) {
 
         fun onBind(item: User) {
             val context = binding.root.context
             val density = context.resources.displayMetrics.density
 
-            isModeActive.observeForever { active ->
-                with(binding) {
-                    val layoutParams =
-                        userPhotoView.layoutParams as ViewGroup.MarginLayoutParams
-                    root.setBackgroundResource(
-                        if (active) R.drawable.selected_user_background
-                        else R.drawable.item_background_selector
-                    )
-                    layoutParams.marginStart =
-                        if (active) (MULTISELECT_MODE_MARGIN * density).toInt()
-                        else (DEFAULT_MARGIN * density).toInt()
-                    userPhotoView.layoutParams = layoutParams
-                    deleteUserView.visibility = if (active) View.GONE else View.VISIBLE
-                    checkBox.visibility = if (active) View.VISIBLE else View.GONE
-                    checkBox.isChecked = item.isSelected
+            with(binding) {
+                val layoutParams =
+                    userPhotoView.layoutParams as ViewGroup.MarginLayoutParams
+                root.setBackgroundResource(
+                    if (isModeActive) R.drawable.selected_user_background
+                    else R.drawable.item_background_selector
+                )
+                layoutParams.marginStart =
+                    if (isModeActive) (MULTISELECT_MODE_MARGIN * density).toInt()
+                    else (DEFAULT_MARGIN * density).toInt()
+                userPhotoView.layoutParams = layoutParams
+                deleteUserView.visibility = if (isModeActive) View.GONE else View.VISIBLE
+                checkBox.visibility = if (isModeActive) View.VISIBLE else View.GONE
+                checkBox.isChecked = item.isSelected
 
-                    userNameView.text = item.name
-                    userCareerView.text = item.career
+                userNameView.text = item.name
+                userCareerView.text = item.career
 
-                    itemView.setOnLongClickListener {
-                        setModeActive(true)
-                        actionListener.onMultiSelectModeActive()
+                itemView.setOnLongClickListener {
+                    changeModeStatus(true)
+                    actionListener.onMultiSelectModeActive()
+                    actionListener.onSelectUser(item)
+                    true
+                }
+                itemView.setOnClickListener {
+                    if (isModeActive) {
                         actionListener.onSelectUser(item)
-                        true
-                    }
-                    itemView.setOnClickListener {
-                        if (active) {
-                            actionListener.onSelectUser(item)
-                        } else {
-                            actionListener.onUserDetails(item)
-                        }
-                    }
-                    deleteUserView.setOnClickListener {
-                        actionListener.onDeleteUser(item)
+                    } else {
+                        actionListener.onUserDetails(item)
                     }
                 }
-                loadImage(binding.userPhotoView, item.photo)
+                deleteUserView.setOnClickListener {
+                    actionListener.onDeleteUser(item)
+                }
             }
+            loadImage(binding.userPhotoView, item.photo)
         }
     }
 
@@ -91,7 +86,7 @@ class UsersAdapter(private val actionListener: UserActionListener) :
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UsersViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val binding = UserPatternBinding.inflate(inflater, parent, false)
-        return UsersViewHolder(binding, actionListener, isModeActive, this::changeModeStatus)
+        return UsersViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: UsersViewHolder, position: Int) {
